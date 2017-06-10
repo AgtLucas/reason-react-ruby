2f00a216b3c23ac7c9c875e9e5b0b5a0a64b4553
AgtLucas <lucas@agtlucas.com>
2017-06-10 10:50:46 +0200

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: './lib/js/src/ruby.js',
+  entry: './lib/js/src/reasonReactRuby.js',
 
   output: {
     path: path.join(__dirname, 'bundleOutputs'),
